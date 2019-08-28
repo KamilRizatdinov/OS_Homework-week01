2aabba9935be530a2bcdb7b8d3357661d4d56245
KamilRizatdinov <km.rizatdinov@gmail.com>
2019-08-28 18:11:03 +0300

    Added week2 lab tasks

--- week02/ex4.c
+++ week02/ex4.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-#include <string.h>
 
-char** swap(char* arr[]) {
-    char* tmp = arr[1];
-    arr[1] = arr[2];
-    arr[2] = tmp;
-    return arr;
+void swap(int* num1, int* num2) {
+    int tmp;
+    tmp = *num1;
+    *num1 = *num2;
+    *num2 = tmp;
 }
 
-int main(int argc, char* argv[]) {
-    char** string = swap(argv);
-    printf("%s %s", string[1], string[2]);
+int main() {
+    int num1;
+    int num2;
+
+    scanf("%d %d", &num1, &num2);
+    swap(&num1, &num2);
+    printf("%d %d", num1, num2);
     return 0;
 }
